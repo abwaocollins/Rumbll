d8330e65fc2f4ca971332169ad16ad872b792ba3
abwaocollins <abwaocollins060@gmail.com>
2022-09-23 20:30:40 +0300

    Rumbl project files

---
+++ test/support/fixtures/multimedia_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule Rumbl.MultimediaFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Rumbl.Multimedia` context.
+  """
+
+  @doc """
+  Generate a video.
+  """
+  def video_fixture(attrs \\ %{}) do
+    {:ok, video} =
+      attrs
+      |> Enum.into(%{
+        url: "http://example.com"
+      })
+      |> Rumbl.Multimedia.create_video()
+
+    video
+  end
+end
